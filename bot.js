4483a37c29496a95e6d3f7967605a6ba11e03320
iOwnYouRBX <33134643+iOwnYouRBX@users.noreply.github.com>
2017-10-26 23:51:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ bot.on("message", function(message) {
 
                 var mentionlist = message.mentions.members;
                 mentionlist.forEach(function (user) {
-                    setTimeout(null,250)
+                    setTimeout('',250)
                     message.channel.send(user.user.username+ " , You have been ranked up! ");
                     user.addRole("202542658634252289");
                 });
