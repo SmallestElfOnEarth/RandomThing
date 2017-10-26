f319182b67655b772792abd4251e98b1abb8cb3d
iOwnYouRBX <33134643+iOwnYouRBX@users.noreply.github.com>
2017-10-26 22:30:15 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js"); 
+const TOKEN = "MzQyNjYxNzI4MTc1MjU5NjQ5.DGS4Jg.EjbL-_QR1AnRDgosj4PBB5qPOLc"
+const PREFIX = ">>"
+var bot = new Discord.Client();
+
+bot.on("ready", function(){
+    console.log("Ready");
+    bot.user.setGame("here comes iown");
+});
+
+bot.on("message", function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if (!message.content.startsWith(PREFIX)) return;
+    
+    var args = message.content.substring(PREFIX.length).split(" ")
+
+    switch (args[0].toLowerCase()) {
+        case "waddup":
+            message.channel.send("ay whassup my nigga im da bot")
+            break;
+        case "info":
+            message.channel.send("ay waddup iown is my creator i worship him, also if you read his name you automatically become his personal property.")
+            break;
+        case "asseating":
+            message.channel.send("if you wanna know how to eat ass you should contact godstatus");
+            break;
+        case "rankup":
+            console.log(args.length);
+            let role = message.guild.roles.find("name", "Staff Assistant | Bitches");
+            if (message.member.roles.has(role.id)) {
+
+                var mentionlist = message.mentions.members;
+                mentionlist.forEach(function (user) {
+                    message.channel.send(user.user.username+ " , You have been ranked up! ");
+                    user.addRole("202542658634252289");
+                });
+                
+              
+
+            }
+            break;
+        default:
+            message.channel.send("no such command bro")
+    }
+});
+
+bot.login(TOKEN);
