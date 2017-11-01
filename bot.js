4991f707192bfd5ab5202da743cfe26d55696726
iOwnYouRBX <33134643+iOwnYouRBX@users.noreply.github.com>
2017-11-01 22:05:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ function autoannounce(){
     var hours = date.getHours();
     var mins = date.getMinutes();
     var challenge = "!challenge";
-    if (day == 1 || day == 3 || day == 5 && hours == 23 && mins == 1)
+    if (day == 1 || day == 3 || day == 5 && hours == 23 && mins < 1)
         channel.send("**Weekly challenge submissions due every Sunday! Type " + challenge + " in #bot-commands  to see what this week's gun is.**");
  }
     setInterval(autoannounce,60000);
