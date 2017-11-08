68c56bcd8c965a5393412065e4d074e8be087b7a
iOwnYouRBX <33134643+iOwnYouRBX@users.noreply.github.com>
2017-11-09 01:12:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ function autoannounce(){
         console.log("day = true " + "Hour: " + hours + " Minutes: "+ mins);
         if( hours === 23 && mins == 1){
             console.log("Hour: " + hours +" Minutes: "+ mins);
-        channel.send("**Weekly challenge submissions due every Sunday! Type " + challenge + " in ** <#202547037667983360> ** to see what this week's gun is.**");
+        channel.send("**Weekly challenge submissions due every Sunday! Type " + challenge + " in ** <#235888338366758912> ** to see what this week's gun is.**");
  }}}
     setInterval(autoannounce,60000);
 
