f68c1a72530e415c5b0e8fa6a11ae9b2baef0e17
iOwnYouRBX <33134643+iOwnYouRBX@users.noreply.github.com>
2017-11-21 01:28:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ function PullLeaderboard_WithName(time, startindex,name) {
         Object.keys(body).map((key) => {
             //clanfound.push(body[key].Name);
             if (body[key].Name == name) {
-                playerfound = "The score for " +name+ " is: " + body[key].FullPoints);
+                playerfound = "The score for " +name+ " is: " + body[key].FullPoints;
             }
            // console.log(number + ".Name: " + boi[key].Name + " Score:" + boi[key].FullPoints + " Position:" + boi[key].Rank);
           //  number++;
