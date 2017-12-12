07ab6613bdc50ea70588a2160db5aba985b94174
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2017-12-12 13:48:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -211,7 +211,7 @@ bot.on("message", function(message) {
                 const filteredArray = arr.filter(function(x) { 
                  return clanfound.indexOf(x) < 0;
                 });
-                const filteredArray = arrog.filter(function(x) { 
+                 filteredArray = arrog.filter(function(x) { 
                  return clanfound.indexOf(x) < 0;
                 });
                 message.channel.send("List of users the scanner couldnt find, Meaning they are position 27,500 or more\nOr they have a score of " + scorereq + " or less : \n **"+ arr.join("\n") + "**");
