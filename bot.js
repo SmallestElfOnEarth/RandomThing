99eb18f7b3efd6078ae94ae4845ba9118eb45e68
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2017-12-12 13:06:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,6 +80,7 @@ function PullLeaderboard(time, startindex) {
             clanfound.push(boi[key].Name);
             if (boi[key].Points > scorereq) {
                  clanfound.push(boi[key].Name);
+                console.log(boi+" passed the score req");
             }
          //   console.log(number + ".Name: " + boi[key].Name + " Score:" + boi[key].FullPoints + " Position:" + boi[key].Rank);
             //number++;
