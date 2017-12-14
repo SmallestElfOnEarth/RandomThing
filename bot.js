24403936e986fb22ae96c33b969f9d4c5b40d7c1
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2017-12-14 14:51:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -208,10 +208,8 @@ bot.on("message", function(message) {
         case "validtokick":
             let role3 = message.guild.roles.find("name", "Clan Manager");
             if (message.member.roles.has(role3.id)) {
-                clanfound = clanfound.concat(arrog);
-                var filteredArray = arr.filter(function(x) { 
-                 return clanfound.indexOf(x) < 0;
-                });
+                arr.filter(x => !~clanfound.indexOf(x)))
+             
                 message.channel.send("List of users the scanner couldnt find, Meaning they are position 27,500 or more\nOr they have a score of " + scorereq + " or less : \n **"+ filteredArray.join("\n") + "**");
     
             }
