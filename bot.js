bfe962b01c70f66401e2cca57e47dab86ef0282a
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2018-01-21 15:46:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -209,7 +209,8 @@ bot.on("message", function(message) {
         case "validtokick":
             let role3 = message.guild.roles.find("name", "Clan Manager");
             if (message.member.roles.has(role3.id)) {
-                message.channel.send("List of users the scanner couldnt find, Meaning they are position 27,500 or more\nOr they have a score of " + scorereq + " or less : \n **"+ clanfound.join("\n") + "**");
+             //   message.channel.send("List of users the scanner couldnt find, Meaning they are position 27,500 or more\nOr they have a score of " + scorereq + " or less : \n **"+ clanfound.join("\n") + "**");
+            require('TryThis.js')(1000000, message.channel)                
             }
             else {
                 message.channel.send("Insufficient Permissions.");
