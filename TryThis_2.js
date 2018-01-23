dac89804715a4a27f456ab2dd10b646cef3029e7
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2018-01-24 00:49:00 +0200

    Add files via upload

---
+++ TryThis_2.js
@@ -0,0 +1,99 @@
+var clanMembers = require ('./Path/clanMembers.js');
+var clanIgnore = require ('./Path/clanIgnore.js');
+
+var http = require ('http');
+var timeout = (1000 * 60 * 30); //30 minutes
+var scanDelay = (1000 * 60 * 60); //60 minutes
+
+var httpGet = function (url, callback){
+	http.get (url, function (res){
+		var ret = "";
+		
+		res.on ('data', function (data){
+			ret += data;
+		});
+		res.on ('end', function (){
+			var p = ret;
+			try {
+				p = JSON.parse (ret);
+			} catch (err){
+			    console.log (p);	callback ([]);
+			};
+			
+			try {
+				callback (p);
+			} catch (err) {
+			    console.log (p);	callback (false);
+			};
+		});
+	});
+};
+
+var scan;
+var scannedMembers = [];
+var scanInProgress = false;
+	scan = function (){
+		scannedMembers = [];
+		scanInProgress = true;
+		var expire = Date.now() + timeout;
+		var index = -50;
+		var nextPage;
+			nextPage = function(){
+				index += 50;
+				httpGet ('http://www.roblox.com/leaderboards/game/json?targetType=0&distributorTargetId=113491250&timeFilter=2&startIndex=' +  index + '&currentRank=1&previousPoints=3372937&max=50&imgWidth=48&imgHeight=48&imgFormat=PNG', function (data){
+					if (Date.now() >= expire || data.length == 0 || members.length == clanMembers.length) {
+						scanInProgress = false;
+						setTimeout (scan, scanDelay);
+						return;
+					};
+					
+					data.forEach (function (query){
+						if (query.ClanName == 'Phantom Rangers || Competitive PF Team')
+							scannedMembers.push (query);
+					});
+					nextPage ();
+				});
+			};
+		nextPage();
+	};
+	
+	setTimeout (scan, 0);
+	
+var m = function (points, channel){
+	if (scanInProgress)
+		return channel.send ('Unable to process command, initial scan still in progress');
+	
+	scannedMembers.sort (function (a, b){
+		return (a.Points == b.Points ? 0 : (a.Points < b.Points ? -1 : 1));
+	});
+	
+	var embed = {
+		title: "Members found with less than " + points + " points!",
+		fields: []
+	};
+	
+	scannedMembers.forEach (function (query){
+		if (!clanIgnore.find (j => query.Name.toLowerCase() == j.toLowerCase()) && query.Points < points)
+			embed.fields.push ({
+				name: query.Name + " (" + query.UserId + ")",
+				value: "Points: " + query.Points + " | Rank: " + query.Rank
+			});
+	});
+	
+	clanMembers.forEach (function (userName){
+		if (!clanIgnore.find (j => j.toLowerCase() == userName.toLowerCase()) && !scannedMembers.find (j => j.Name == userName))
+			embed.fields.push ({
+				name: userName,
+				value: "This member was not found on the list, far below required points"
+			});
+	});
+	
+	while (embed.fields.length > 25)
+		embed.fields.unshift (embed.fields.length, 1);
+	
+	channel.send ('', {
+		embed: embed
+	});
+};
+
+module.exports = m;
