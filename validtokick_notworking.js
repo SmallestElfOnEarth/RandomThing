f830c35824091519991c6a1cbce0779596d97959
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2018-02-06 01:26:16 +0200

    Update validtokick_notworking.js

--- validtokick_notworking.js
+++ validtokick_notworking.js
@@ -2,7 +2,7 @@ var clanMembers = require ('./clanmembers.js');
 var clanIgnore = require ('./clanogs.js');
 
 var http = require ('http');
-var timeout = (1000 * 60 * 5); 
+var timeout = (1000 * 60 * 180); 
 var scanDelay = (1000 * 60 * 5); 
 
 var httpGet = function (url, callback){
