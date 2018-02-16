f72d099d91241b3729f61febe8c7760007f1833b
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2018-02-16 17:46:12 +0200

    Create payoutsys.js

---
+++ payoutsys.js
@@ -0,0 +1,8 @@
+var rbx = require('roblox-js');
+var username = 'rpgwill'
+var password = 'rpgwill11'
+module.exports = {
+  payout: function(c,u,user,amount){
+  rbx.groupPayout(2817199,user,amount);
+  c.send('<@' + u.id + '>,"+ amount +" robux has been successfuly paid to "+ user +"!");
+ }
