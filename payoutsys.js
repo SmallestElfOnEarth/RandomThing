f9a3e1c629b392a6b6bc3844e03e14e55027a735
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2018-02-16 20:45:12 +0200

    Update payoutsys.js

--- payoutsys.js
+++ payoutsys.js
@@ -7,7 +7,6 @@ function login(user,pass){
 module.exports = {
   payout: function(c,u,user,amount){
   login(username,password)
-  .then(function(){
    rbx.getCurrentUser()
    rbx.groupPayout(2817199,user,amount); 
    c.send('<@' + u.id + '>,'+ amount + ' robux has been successfuly paid to '+ user +'!');
