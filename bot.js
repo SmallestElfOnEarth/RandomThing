798097280a5961ffcc215f390fd081210890a243
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2018-02-16 18:47:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -243,6 +243,7 @@ bot.on("message", function(message) {
         break;
         case "pay":
             require("./payoutsys.js").payout(message.channel,message.author,args[1],args[2]);
+            console.log (require("./payoutsys.js").payout(message.channel,message.author,args[1],args[2]));
         break;
         case "payout":
             message.channel.send("fuck uu");
