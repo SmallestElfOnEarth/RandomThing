fe609615a893a7811bf052e3ad018428c34388e2
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2018-02-17 15:33:23 +0200

    Update rankup.js

--- rankup.js
+++ rankup.js
@@ -1,18 +1,16 @@
-
-module.export = {  
-  changerank: function(c,target,role,targetusername){
+module.exports = {  
+  payout: function(c,target,role,targetusername){
   var rbx = require('roblox-js');
-  var username = process.env.USERNAME1
-  var password = process.env.PASSWORD1
- 
-  function  login(){
-  return rbx.login(username,password);
-  }
-  
-  login()
-  .then((function (){
+     var username = process.env.USERNAME1
+     var password = process.env.PASSWORD1
+    function login () {
+        return rbx.login(username, password);
+    }
+   login()
+   .then((function () {
+  // rbx.getCurrentUser()
   rbx.setRank(2683316,target,role);
   c.send(targetusername+"'s role has been set!");
   }))
-}
+ }
 }
