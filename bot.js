9f86a4edc0093d18100c2dc17622c8bd8278dd55
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2018-02-19 16:21:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,6 +29,10 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  var sql = "CREATE TABLE transactions (id INT AUTO_INCREMENT PRIMARY KEY, info VARCHAR(255))";
+  connection.query(sql, function(err,result) {
+      if (err) throw err;
+      console.log("Table created");
 });
 
 
