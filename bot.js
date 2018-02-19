f95a35342d688ec2a7d1f36b0ebc8c952c6874b4
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2018-02-19 16:33:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -390,7 +390,7 @@ bot.on("message", function(message) {
                  connection.connect(function(err) {
                  if (err) throw err;
                  console.log("Connected!");
-                 var sql = "INSERT INTO transactions (info) VALUES(logmessage)");
+                 var sql = "INSERT INTO transactions (info) VALUES(logmessage)";
                  connection.query(sql, function (err,result) {
                   if(err) throw err;
                   console.log("1 record inserted");
