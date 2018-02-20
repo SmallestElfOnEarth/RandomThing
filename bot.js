ac788b7a85b531fa323e81d9e5b2fd77f2ad5607
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2018-02-20 09:09:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -228,7 +228,7 @@ bot.on("message", function (message) {
 
         case "rankup":
             if 
-let user= message.mentions.users.first()
+let user= message.mentions.users.first();
 (message.member.roles.has(staffass.id)) {
                 httpGet(`https://api.roblox.com/users/get-by-username?username=${args[1]}`, function (data) {
                     var data = JSON.parse(data);
