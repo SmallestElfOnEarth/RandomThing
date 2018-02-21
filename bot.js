41fa39a841a9d2fc66785d7f887793b358ab4b46
AnonymousRobloxianUser <33134643+SmallestElfOnEarth@users.noreply.github.com>
2018-02-21 17:48:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -127,12 +127,13 @@ connection.query(`CREATE TABLE if not exists transactionslog (
       rbx.getPlayers(groupId, role).then(group => {
         for (let i = 0; i < group.players.length; i++) {
           if (group.players[i].id === userId) {
-            if(convertedRole.nextLevel != 0 && convertedRole.nextLevel != 1){
-            callback(robloxUsername+", you already have that role assigned! Your next rankup is at level" + convertRole(roleFromArgs).nextLevel)
+            if(convertedRole.nextLevel == 0 ){
+                callback(robloxUsername+", Congratulations, You ranked up to the highest rank we currently offer! (200).");
             }else if(convertedRole.nextLevel == 1){
                 callback(robloxUsername+", your rank isn't high enough! the minimum rank available is 20.");
             }else{
-                    callback(robloxUsername+", Congratulations, You ranked up to the highest rank we currently offer! (200).");
+                callback(robloxUsername+", you already have that role assigned! Your next rankup is at level" + convertRole(roleFromArgs).nextLevel)
+
             }
             break;
           }
